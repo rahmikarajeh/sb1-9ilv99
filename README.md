aad56ffa7e2661be3d12ef5db97b7cdaf9830727
rahmikarajeh <rahmikarajeh2@gmail.com>
2024-10-11 20:12:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-9ilv99
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/rahmikarajeh/sb1-9ilv99)
---
+++ functions/api.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const multer = require('multer');
+const Jimp = require('jimp');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.get('/', (req, res) => {
+  res.send('Halftone API is running. Send a POST request to /halftone with an image file to process it.');
+});
+
+app.post('/halftone', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No image file uploaded' });
+    }
+
+    const image = await Jimp.read(req.file.buffer);
+    const { width, height } = image.bitmap;
+
+    // Apply halftone effect
+    image.scan(0, 0, width, height, function(x, y, idx) {
+      const red = this.bitmap.data[idx + 0];
+      const green = this.bitmap.data[idx + 1];
+      const blue = this.bitmap.data[idx + 2];
+      
+      const gray = (red + green + blue) / 3;
+      const size = 1 - gray / 255;
+      const half = size / 2;
+      
+      if ((x % 4 < size) && (y % 4 < size)) {
+        this.bitmap.data[idx + 0] = 0;
+        this.bitmap.data[idx + 1] = 0;
+        this.bitmap.data[idx + 2] = 0;
+      } else {
+        this.bitmap.data[idx + 0] = 255;
+        this.bitmap.data[idx + 1] = 255;
+        this.bitmap.data[idx + 2] = 255;
+      }
+    });
+
+    const processedImageBuffer = await image.getBufferAsync(Jimp.MIME_PNG);
+    res.set('Content-Type', Jimp.MIME_PNG);
+    res.send(processedImageBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing the image' });
+  }
+});
+
+module.exports.handler = serverless(app);
